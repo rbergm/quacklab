d3057dc35c1265d73e2b9bbcdbf3e20eac3ceffd
Maia <maia@duckdblabs.com>
2026-01-14 11:19:04 +0100

    bump extensions

--- .github/config/extensions/vss.cmake
+++ .github/config/extensions/vss.cmake
@@ -2,6 +2,6 @@ duckdb_extension_load(vss
         LOAD_TESTS
         DONT_LINK
         GIT_URL https://github.com/duckdb/duckdb-vss
-        GIT_TAG 9b25336989efdca9598ae90364ce13cc976f2f31
+        GIT_TAG d796205e45ba38f894fb16bbbf1b89374d3be2e5
         TEST_DIR test/sql
     )
